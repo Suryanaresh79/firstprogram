36987e3e6635f701d576b9bce73d161cc69da5d0
Suryanaresh79 <98698880+Suryanaresh79@users.noreply.github.com>
2022-03-07 16:26:10 +0530

    Add files via upload

---
+++ Airthmetic.java
@@ -0,0 +1,13 @@
+public class Demo1 {
+
+	public static void main(String[] args) {
+	int a = 10 , b = 5;
+	System.out.println("a value is : "+a);
+	System.out.println("b value is : "+b);
+	System.out.println("sum is : "+(a+b));
+	System.out.println("substraction is : "+(a-b));
+	System.out.println("muliplication is : "+(a*b));
+	System.out.println("division is : "+(a/b));
+	System.out.println("Modulus is : "+(a%b));
+	}
+}
